b5c7209add0e06f4fa210e781e7a3101efb99547
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2016-03-08 16:11:55 -0800

    Setup base ViewModel and first ViewModel

--- SwarmSharp/ViewModels/PlayfieldViewModel.cs
+++ SwarmSharp/ViewModels/PlayfieldViewModel.cs
@@ -1,11 +1,100 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using SkiaSharp;
 
 namespace SwarmSharp
 {
-	public class PlayfieldViewModel
+	public class PlayfieldViewModel : ViewModel
 	{
-		public PlayfieldViewModel ()
-		{
+		Playfield playfield;
+
+		private Action<SKCanvas, int, int> renderAction;
+		public Action<SKCanvas, int, int> RenderAction{
+			get { return renderAction; }
+			set { SetProperty (ref renderAction, value); }
+		}
+
+		private bool needRedraw;
+		public bool NeedRedraw {
+			get { return needRedraw; }
+			set { SetProperty (ref needRedraw, value); }
+		}
+
+		public int Width { 
+			get { return playfield.Width; } 
+			set { playfield.Width = value; }
+		}
+
+		public int Height {
+			get { return playfield.Height; }
+			set { playfield.Height = value; }
+		}
+
+		public PlayfieldViewModel () {
+			int numAgents = 1000;
+			NeedRedraw = false;
+
+			playfield = new Playfield ();
+			RenderAction = new Action<SKCanvas, int, int> (render);
+			//playfield.Width = (int)skiaView.Width;
+			//playfield.Height = (int)skiaView.Height;
+
+			Debug.WriteLine ("Width height: {0}, {1}", playfield.Width, playfield.Height);
+
+			var random = new Random ();
+			var agents = new List<Agent> ();
+			for (int i = 0; i < numAgents; i++) {
+				var agent = new Agent ();
+				agents.Add (agent);
+			}
+			var agentArray = agents.ToArray ();
+			foreach (var agent in agentArray) {
+				var rule = new BuddyBuddy (agent, agentArray [random.Next () % numAgents], agentArray [random.Next () % numAgents]);
+				agent.AddRule (rule);
+			}
+			playfield.AddGroup (agentArray);
+
+			playfield.RePosition ();
+		}
+
+		public void Reset () {
+			playfield.RePosition ();
+		}
+
+		public void Play() {
+			playfield.Iterate ();
+			NeedRedraw = true;
+		}
+
+
+		void render(SKCanvas canvas, int width, int height){
+			canvas.Clear (SKColors.Green);
+			using (var paint = new SKPaint ()) {
+				paint.IsAntialias = true;
+				paint.Color = new SKColor (0x2c, 0x3e, 0x50);
+				paint.StrokeCap = SKStrokeCap.Round;
+
+				if (playfield != null && playfield.Groups != null) {
+					foreach (var group in playfield.Groups) {
+						foreach (var agent in group) {
+							using (var path = new SKPath ()) {
+								int X = agent.Position.X;
+								int Y = agent.Position.Y;
+								path.MoveTo (X, Y);
+								path.LineTo (X + 5, Y);
+								path.LineTo (X + 5, Y + 5);
+								path.LineTo (X, Y + 5);
+								path.Close ();
+
+								// draw the Xamagon path
+								canvas.DrawPath (path, paint);
+							}
+						}
+					}
+				}
+			}
+			NeedRedraw = false;
 		}
 	}
 }
--- SwarmSharp/ViewModels/ViewModel.cs
+++ SwarmSharp/ViewModels/ViewModel.cs
@@ -1,11 +1,27 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SwarmSharp
 {
-	public class ViewModel
+	public class ViewModel : INotifyPropertyChanged
 	{
-		public ViewModel ()
-		{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public ViewModel () { }
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string name = null) {
+			var handler = PropertyChanged;
+			if (handler != null)
+				handler (this, new PropertyChangedEventArgs (name));
+		}
+
+		protected bool SetProperty<T>(ref T data, T value, [CallerMemberName] string name = "") {
+			if (Object.Equals (data, value))
+				return false;
+			data = value;
+			OnPropertyChanged (name);
+			return true;
 		}
 	}
 }
