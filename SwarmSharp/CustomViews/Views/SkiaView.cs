747758f6613df47dd5c35bc43c85036e370ea98f
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2016-03-08 16:11:33 -0800

    Setup custom bindable properties

--- SwarmSharp/CustomViews/Views/SkiaView.cs
+++ SwarmSharp/CustomViews/Views/SkiaView.cs
@@ -6,12 +6,25 @@ namespace SwarmSharp
 {
 	public class SkiaView : View, ISkiaViewController
 	{
-		Action <SKCanvas, int, int> onDrawCallback;
+		public static readonly BindableProperty DrawCallbackProperty = 
+			BindableProperty.Create(nameof(DrawCallback), typeof(Action<SKCanvas, int, int>), typeof(SkiaView), null);
+
+		public static readonly BindableProperty NeedRedrawProperty = 
+			BindableProperty.Create(nameof(NeedRedraw), typeof(bool), typeof(SkiaView), false);
+
+		public bool NeedRedraw {
+			get { return (bool)GetValue (NeedRedrawProperty); }
+			set { SetValue (NeedRedrawProperty, value); }
+		}
+
+		public Action<SKCanvas, int, int> DrawCallback {
+			get { return (Action<SKCanvas, int, int>)GetValue (DrawCallbackProperty); }
+			set { SetValue (DrawCallbackProperty, value); }
+		}
+
 		Action onRedrawCallback;
 
-		public SkiaView (Action <SKCanvas, int, int> onDrawCallback) {
-			this.onDrawCallback = onDrawCallback;
-		}
+		public SkiaView () { } 
 
 		void ISkiaViewController.SendDraw (SKCanvas canvas) {
 			Draw (canvas);
@@ -21,13 +34,33 @@ namespace SwarmSharp
 			this.onRedrawCallback = recallCallback;
 		}
 
-		void ISkiaViewController.Redraw (){
+		public void Redraw(){
 			Device.BeginInvokeOnMainThread (() =>
 				onRedrawCallback ());
 		}
 
 		protected virtual void Draw (SKCanvas canvas) {
-			onDrawCallback (canvas, (int) Width, (int) Height);
+			if (DrawCallback != null)
+				DrawCallback (canvas, (int) Width, (int) Height);
+		}
+
+		protected override void OnBindingContextChanged ()
+		{
+			base.OnBindingContextChanged ();
+
+			if (BindingContext is PlayfieldViewModel) {
+				((PlayfieldViewModel)BindingContext).Width = (int)Width;
+				((PlayfieldViewModel)BindingContext).Height = (int)Height;
+				((PlayfieldViewModel)BindingContext).Reset ();
+			}
+		}
+
+		protected override void OnPropertyChanged (string propertyName)
+		{
+			base.OnPropertyChanged (propertyName);
+
+			System.Diagnostics.Debug.WriteLine (propertyName + " Property changed!");
+			Redraw ();
 		}
 	}
 }
