7cdd7d4a3d1acb5f674a053064018ce61da3b56c
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2016-03-15 23:57:41 -0500

    Setup OpenGL Structure

---
+++ SwarmSharp/CustomViews/Views/SwarmView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace SwarmSharp
+{
+	public class SwarmView : ContentView
+	{
+		OpenGLView glView = new OpenGLView ();
+
+		public SwarmView ()
+		{
+			var engine = DependencyService.Get<IGLEngine> ();
+			glView.OnDisplay = engine.Render;
+			Content = glView;
+			glView.HasRenderLoop = true;
+			glView.Display ();
+		}
+	}
+}
+
+
