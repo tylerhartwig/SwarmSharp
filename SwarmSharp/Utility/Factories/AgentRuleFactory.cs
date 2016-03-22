608eb643d0af07ce873857a9a08043bd77f062b7
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2016-03-22 10:10:40 -0500

    Create factory

--- SwarmSharp/Utility/Factories/AgentRuleFactory.cs
+++ SwarmSharp/Utility/Factories/AgentRuleFactory.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+using Xamarin.Forms;
+using System.Linq;
 
 namespace SwarmSharp
 {
 	public class AgentRuleFactory
 	{
-		public AgentRuleFactory ()
-		{
+		// Singleton
+		private static AgentRuleFactory instance;
+		public static AgentRuleFactory Instance {
+			get { 
+				if (instance == null)
+					instance = new AgentRuleFactory ();
+				return instance;
+			}
 		}
+
+		private Dictionary<string, Type> rules = new Dictionary<string, Type> ();
+
+		private AgentRuleFactory () {
+			var assembly = typeof(AgentRuleFactory).GetTypeInfo ().Assembly;
+			var types = assembly.DefinedTypes;
+			foreach (var type in types) {
+				var attribute = type.GetCustomAttributes().Where (a => a is AgentRule).FirstOrDefault ();
+				if(attribute != null)
+					rules.Add (((AgentRule)attribute).Name, type.AsType ());
+			}
+		}
+
+		public List<string> GetRuleNames () {
+			return rules.Keys.ToList ();
+		}
+
+		public IAgentRule CreateRule (string name) {
+			var rule = rules [name];
+			return Activator.CreateInstance (rule) as IAgentRule;
+		}
+
 	}
 }
 
