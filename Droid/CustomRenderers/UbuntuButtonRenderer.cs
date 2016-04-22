b0c501ae546a9df033b293a6c98bf951f02cc2fc
Tyler Hartwig <Tyler_hartwig@baylor.edu>
2016-04-22 10:58:34 -0500

    Implement Custom Fonts on android

---
+++ Droid/CustomRenderers/UbuntuButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using Android.Widget;
+using Android.Graphics;
+using Xamarin.Forms;
+using SwarmSharp.Droid;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(UbuntuButtonRenderer))]
+namespace SwarmSharp.Droid
+{
+	public class UbuntuButtonRenderer : ButtonRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.Button> e)
+		{
+			base.OnElementChanged (e);
+
+			var button = (Android.Widget.Button)Control;
+			Typeface font = Typeface.CreateFromAsset (Forms.Context.Assets, "Ubuntu-L.ttf");
+			button.Typeface = font;
+		}
+	}
+}
+
---
+++ Droid/CustomRenderers/UbuntuLabelRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms.Platform.Android;
+using Android.Widget;
+using Android.Graphics;
+using Xamarin.Forms;
+using SwarmSharp.Droid;
+
+[assembly: ExportRenderer (typeof (Label), typeof(UbuntuLabelRenderer))]
+namespace SwarmSharp.Droid
+{
+	public class UbuntuLabelRenderer : LabelRenderer
+	{
+		protected override void OnElementChanged (ElementChangedEventArgs<Xamarin.Forms.Label> e)
+		{
+			base.OnElementChanged (e);
+			var formsElement = e.NewElement;
+			System.Diagnostics.Debug.WriteLine (formsElement.FontFamily);
+			var label = (TextView)Control;
+			Typeface font = Typeface.CreateFromAsset (Forms.Context.Assets, "Ubuntu-L.ttf");
+			label.Typeface = font;
+		}
+	}
+}
+
